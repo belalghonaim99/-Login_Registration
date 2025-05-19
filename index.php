b9dc29555923702afd91ace8eae5a9497b87730f
BelalGhonaim <belalpop1999@yahoo.com>
2025-05-20 01:58:00 +0300

    updated indexfile

--- index.php
+++ index.php
@@ -17,42 +17,5 @@ session_start();
 <body>
     <div class="container">
         <h1>Welcome To Your Dashboard</h1>
-        <p>Here You Can Add Books </p>
-        <a href="logout.php" class="btn btn-warning">logout</a>
-    </div>
-        <div class="container">
-        <header class="d-flex justify-content-between my-4">
-            <h1>Add New Book</h1>
-            <div>
-                <!-- will back to add link -->
-                <a href="" class="btn btn-primary">Back</a>
-            </div>
-        </header>
-        <form action="" method="post">
-            <div class="form-element my-4">
-                <input type="text" class="form-control" name="title" placeholder="Book Title:">
-            </div>
-            <div class="form-elemnt my-4">
-                <input type="text" class="form-control" name="author" placeholder="Author Name:">
-            </div>
-
-            <div class="form-element my-4">
-                <select name="type" class="form-control">
-                    <option value="">Select Book Type</option>
-                    <option value="Adventure">Adventure</option>
-                    <option value="Fantasy">Fantasy</option>
-                    <option value="Horror">Horror</option>
-                    <option value="Scifi">Scifi</option>
-                </select>
-            </div>
-
-            <div class="form-element my-4">
-                <input type="text" class="form-control" name="description" placeholder="Book Description:">
-            </div>
-
-            <div class="form-element">
-                <input type="submit" class="btn btn-primary" name="create" value="Add Book">
-            </div>
-
 </body>
 </html>
